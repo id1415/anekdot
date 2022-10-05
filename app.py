1140634d9e82c34742baa72e6242859ac6b6fea3
id1415 <id1415@yandex.ru>
2022-10-05 18:01:04 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import sqlite3
+from flask import Flask, render_template
+from random import randint
+
+
+def get_db_connection():
+    conn = sqlite3.connect('anek.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('select * from anekdot order by random()').fetchmany(10)
+    conn.close()
+    return render_template('index.html', posts=posts)
