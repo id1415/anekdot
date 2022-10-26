8873bcfaf3b1319a774638b1b822c3742e8669f8
id1415 <id1415@yandex.ru>
2022-10-26 19:16:34 +0500

    правки

--- apps.py
+++ apps.py
@@ -1,11 +1,26 @@
 from json import load
+from random import randint
 
 data = load(open('anekdot.json', encoding='utf-8'))  # загрузка json файла с анекдотами
+
 
 class Search:
     def __init__(self, query):
         self.query = query
 
-    def status(self):  # ввёл ли пользователь запрос в поиске
-        if self.query != None and self.query != ' ' and 100 > len(self.query) > 2:
+    def status(self):  # ввёл ли пользователь запрос в поиск
+        if self.query != None and self.query != ' ' and 101 > len(self.query) > 2:
             return True
+
+
+def random_anekdot():
+    id = []
+    anekdots = []
+    for _ in range(10):  # выбираются 10 случайных чисел от 1 до 111707
+        random_number = randint(1, 111707)
+        id.append(random_number)
+    for i in id:  # добавляются анекдоты с id - случайными числами
+        newline = data[i]["anekdot"]
+        anekdots.append(f'{i}\n{newline}')
+    
+    return anekdots
