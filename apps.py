b3f667e692233e03962b34e04a23ddedd7bf8101
id1415 <id1415@yandex.ru>
2022-11-03 19:05:18 +0500

    правки

--- apps.py
+++ apps.py
@@ -1,19 +1,48 @@
 from json import load
 from random import randint
+from flask_paginate import Pagination, get_page_args
 
 data = load(open('anekdot.json', encoding='utf-8'))  # загрузка json файла с анекдотами
 
 
 class Search:
-    def __init__(self, query):
+
+    def __init__(self, query=None):
         self.query = query
 
     def status(self):  # ввёл ли пользователь запрос в поиск
         if self.query != None and self.query != ' ' and 101 > len(self.query) > 2:
             return True
 
+    # поиск и пагинация результатов
+    def search(self):
+        results = []  # сюда помещаются результаты поиска
+        for anekdot in data:
+            if self.query.lower() in anekdot['anekdot'].lower():
+                results.append({anekdot['id']: anekdot['anekdot']})
+
+        total = len(results)  # количество найденных анекдотов
+
+        # page - номер страницы
+        # per_page - результатов на страницу
+        # offset = (page - 1) * per_page
+        page, per_page, offset = get_page_args(page_parameter='page',
+                                            per_page_parameter='per_page')
+        
+        results = results[offset: offset + 10]  # список из 10 анекдотов
+
+        pagination = Pagination(page=page,
+                                per_page=per_page,
+                                total=total,
+                                css_framework='Bootstrap3',
+                                display_msg=f'Найдено {total} анекдотов'
+                                )
+
+        return results, pagination
+
 
 def random_anekdot():
+
     anekdots = []
     for _ in range(10):
         random_number = randint(1, 111707)  # выбирается случайное число от 1 до 111707
--- index.py
+++ index.py
@@ -1,60 +1,56 @@
 from flask import Flask, render_template, request
-from flask_paginate import Pagination, get_page_args
-from apps import data, Search, random_anekdot
+from apps import Search, random_anekdot
 
 
 app = Flask(__name__)
+exm = Search()
+menu = [{'name': 'ОБНОВИТЬ', 'url': '/'},
+        {'name': 'О САЙТЕ', 'url': 'about'},
+        ]
 
 
-# результаты поиска и пагинация
-def results(q):
-    results = []  # сюда помещаются результаты поиска
-    for anekdot in data:
-        if q.lower() in anekdot['anekdot'].lower():
-            results.append({anekdot['id']: anekdot['anekdot']})
+# результаты поиска
+def results():
+    search = exm.search()
 
-    total = len(results)  # количество найденных анекдотов
-
-    # page - номер страницы
-    # per_page - результатов на страницу
-    # offset = (page - 1) * per_page
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                        per_page_parameter='per_page')
-    
-    results = results[offset: offset + 10]  # список из 10 анекдотов
-
-    pagination = Pagination(page=page,
-                            per_page=per_page,
-                            total=total,
-                            css_framework='Bootstrap3',
-                            display_msg=f'Найдено {total} анекдотов'
-                            )
-
-    return render_template('results.html', 
-                results=results,
-                pagination=pagination
+    return render_template('results.html',
+                results=search[0],
+                pagination=search[1],
+                menu=menu,
                 )
 
 
 @app.route('/')
 def index():
 
-    q = Search(request.args.get('q'))  # запрос в поиске
-    if q.status():
-        return results(q.query)
+    global exm
+    q = request.args.get('q')  # запрос в поиске
+    exm = Search(q)
+    if exm.status():
+        return results()
 
     anekdots = random_anekdot()
-    return render_template('index.html', anekdots=anekdots)
+    return render_template('index.html',
+                            anekdots=anekdots,
+                            menu=menu,
+                            title='Анекдоты',
+                            )
 
 
 @app.route('/about')
 def about():
 
-    q = Search(request.args.get('q'))  # запрос в поиске
-    if q.status():
-        return results(q.query)
+    # копипаст ¯\_(ツ)_/¯
+    global exm
+    q = request.args.get('q')  # запрос в поиске
+    exm = Search(q)
+    if exm.status():
+        return results()
 
-    return render_template('about.html')
+    return render_template('about.html',
+                            menu=menu, 
+                            title='О сайте',
+                            )
 
 
 if __name__ == '__main__':
